8d1885a6fac31a2b6bddc62e1b27a6a8edbf5349
Eduardo de Andrade <eduardo.ufg@hotmail.com>
2021-09-10 01:40:51 -0300

    experiments 2

--- ga2.py
+++ ga2.py
@@ -13,30 +13,38 @@ ELITISM = True
 
     
 par = [
-       {"pop_size": 100,
+       {0"pop_size": 200,
         "num_ger": NUM_GER,
         "bits": BITS,
-        "crossover_rate": 0.65,
-        "mutation_rate": 0.008,
+        "crossover_rate": 0.60,
+        "mutation_rate": 0.0015,
         "elitism": ELITISM},
        {"pop_size": 200,
         "num_ger": NUM_GER,
         "bits": BITS,
-        "crossover_rate": CROSSOVER_RATE,
-        "mutation_rate": 0.005,
-        "elitism": True},
+        "crossover_rate": 0.70,
+        "mutation_rate": 0.0015,
+        "elitism": ELITISM},
        {"pop_size": 200,
         "num_ger": NUM_GER,
         "bits": BITS,
-        "crossover_rate": 0.65,
+        "crossover_rate": 0.75,
+        "mutation_rate": 0.0015,
+        "elitism": ELITISM},
+       {"pop_size": 200,
+        "num_ger": NUM_GER,
+        "bits": BITS,
+        "crossover_rate": 0.55,
         "mutation_rate": 0.0015,
         "elitism": ELITISM}]   
 
 
 
-y = [0,0,0]
 
-for j in range(0,3):
+
+y = [0,0,0,0]
+
+for j in range(0,4):
     params_ = par[j];
     for i in range(0,1000):
         apt, generation,crom,max = genetic_algorithm_algorithm(params_)
@@ -44,13 +52,15 @@ for j in range(0,3):
             y[j] += 1
     
 
-names = ["Mutation: 0.008 Pop:100","Mutation: 0.005 Pop:200", "Mutation: 0.0015 Pop:200"]
+names = ["Mut: 0.0015 cros: 0.60","Mut: 0.0015 cros: 0.70", "Mut: 0.0015 cros: 0.75","Mut: 0.0015 cros: 0.55"]
 
 y[0] = y[0]/1000
 y[1] = y[1]/1000
 y[2] = y[2]/1000
+y[3] = y[3]/1000
 
 plt.title("Eficácia média em 1000 execuções")
       
-plt.savefig("teste.png")
+
 plt.bar(names, y)
+plt.savefig("teste.png")
